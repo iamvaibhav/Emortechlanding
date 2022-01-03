710a69ec5a687a46eb97d077b19b753d229728fa
aakritimehraa <aakritimehrasa@gmail.com>
2022-01-03 17:39:51 +0530

    added testimonials

--- src/components/Home/SectionThree.js
+++ src/components/Home/SectionThree.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import '../../styles/SectionThree.css'
+import React from "react"
+import "../../styles/SectionThree.css"
 
 function SectionThree() {
-    return (
-        <div className='section_three'>
-            
+  return (
+    <div className="section_three">
+      <h1>
+        <span>Dont believe us?</span> See what our clients say
+      </h1>
+      <section>
+        <div>
+          <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa eu,
+            lectus ac venenatis dolor. Morbi lectus pellentesque augue.
+          </h2>
+          <div className="section_three_right_div">
+            <p>Client Name</p>
+            <p>Company name</p>
+          </div>
         </div>
-    )
+
+        <div>
+          <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa eu,
+            lectus ac venenatis dolor. Morbi lectus pellentesque augue.
+          </h2>
+          <div className="section_three_right_div">
+            <p>Client Name</p>
+            <p>Company name</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default SectionThree
