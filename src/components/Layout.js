8b84224e56fd82b4db58dcf709dd219afe076089
aakritimehraa <aakritimehrasa@gmail.com>
2022-01-08 15:18:11 +0530

    corrected deploying errors

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import '../styles/Header.css'
 import Footer from './Footer'
-import '../styles/Footer.css'
 
 function Layout(props) {
     return (
