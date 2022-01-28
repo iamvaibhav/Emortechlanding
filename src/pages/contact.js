0bc42cbc7a3efab36ae0b9fc7dc927b7987b1c77
aakritimehraa <aakritimehrasa@gmail.com>
2022-01-28 14:12:14 +0530

    corect deploy errors

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,9 +15,9 @@ function contact() {
         <section className='flex flex-wrap-reverse justify-center lg:justify-between items-center'>
           <div className='flex flex-col sm:justify-center items-center min-h-[800px] w-[100%] sm:w-[800px]'>
             <form className='max-w-[300px] w-[90%] sm:max-w-[600px]'>
-              <input className='h-[40px] w-[90%] sm:w-[600px] bg-secondary border-b-2 border-[#1382C7] mt-10 p-2' placeholder='First Name'  type='text' />
-              <input className='h-[40px] w-[90%] sm:w-[600px] bg-secondary border-b-2 border-[#1382C7] mt-10 p-2' placeholder='Last Name' type='text' />
-              <input className='h-[40px] w-[90%] sm:w-[600px] bg-secondary border-b-2 border-[#1382C7] mt-10 p-2' placeholder='Company Name' type='text' />
+              <input className='h-[40px] w-[90%] sm:w-[600px] text-primary bg-secondary border-b-2 border-[#1382C7] mt-10 p-2' placeholder='First Name'  type='text' />
+              <input className='h-[40px] w-[90%] sm:w-[600px] text-primary bg-secondary border-b-2 border-[#1382C7] mt-10 p-2' placeholder='Last Name' type='text' />
+              <input className='h-[40px] w-[90%] sm:w-[600px] text-primary bg-secondary border-b-2 border-[#1382C7] mt-10 p-2' placeholder='Company Name' type='text' />
               <p className='text-primary mt-5 mb-2'>What type of service are you looking for?</p>
               <ul className='max-w-[600px] px-2 flex flex-wrap justify-start items-center '>
                 <li className='h-[35px] mt-2 text-primary bg-secondary border-2 border-primary mr-2 px-5 flex justify-center items-center text-[12px] sml:text-[18px] leading-[24px] rounded-[10px]'><input className='mr-5' type='radio' /> AI/ML</li>
