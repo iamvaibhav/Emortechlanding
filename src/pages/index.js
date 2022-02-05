f85daf63b9ac7197070ba0e6dac84a17a152d396
aakritimehraa <aakritimehrasa@gmail.com>
2022-02-05 15:34:49 +0530

    fix menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,18 @@ import Bannerhome from '../components/Home/Bannerhome'
 import Layout from '../components/Layout'
 import '../styles/Index.css'
 import '../styles/Banner.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { ThemeProvider } from '../components/themeContext'
 
 
 function index() {
   return (
-     <Layout>
-       <Bannerhome  />
-     </Layout>
+    <ThemeProvider>
+    <Header />
+    <Bannerhome />
+    <Footer />
+    </ThemeProvider>
   )
 }
 
